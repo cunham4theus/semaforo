f269aef31f62f80144e9058b8de3c084cfff3952
cunham4theus <cunhamatheus.m4@gmail.com>
2022-12-26 05:23:25 -0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = (event) => {
+  stopAutomatic();
+  turnOn[event.target.id]();
+
+};
+
+const nextIndex = () => (colorIndex = colorIndex < 2 ? ++colorIndex : 0);
+
+const changeColor = () => {
+  const colors = ['red', 'yellow', 'green'];
+  const color = colors[colorIndex];
+  turnOn[color]();
+  nextIndex();
+};
+
+const stopAutomatic = () => {
+  clearInterval(intervalId);
+};
+
+const turnOn = {
+  red: () => (img.src = '/assets/img/vermelho.png'),
+  yellow: () => (img.src = '/assets/img/amarelo.png'),
+  green: () => (img.src = '/assets/img/verde.png'),
+  automatic: () => (intervalId = setInterval(changeColor, 1000)),
+};
+buttons.addEventListener('click', trafficLight);
